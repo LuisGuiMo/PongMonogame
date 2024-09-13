e40cdb867e3b2ff915fadb268990d0aa112404b3
LuisGmolinaC <xdevnon@gmail.com>
2024-09-13 10:03:33 -0500

    Refactor ball logic into new Ball class
    Refactored `Game1.cs` to move ball handling logic into a new `Ball` class. This includes:
    - Replacing old ball-related variables with a `Ball` object.
    - Moving screen resolution settings to the constructor.
    - Updating `Draw` and `Update` methods to use the `Ball` class.
    - Loading ball texture and initializing `Ball` in `LoadContent`.
    - Playing background music using `MediaPlayer`.
    
    Created `Ball.cs` to encapsulate ball behavior:
    - Defined `Ball` class with properties for texture, position, speed, and velocity.
    - Initialized ball position at screen center with diagonal velocity.
    - Implemented `Draw` and `Update` methods for rendering and collision detection.

--- Game1.cs
+++ Game1.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using PongMonogame.Models;
 
 namespace PongMonogame
 {
@@ -9,17 +10,15 @@ namespace PongMonogame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private Texture2D ball2Texture;
-        private Vector2 ballPosition;
-        private float ballSpeed;
-        private Texture2D ballTexture;
-
-        // Audio objects
-        private SoundEffect soundEffect;
+        private Ball ball;
+        private Song song;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 2000;
+            _graphics.PreferredBackBufferHeight = 1000;
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -27,22 +26,8 @@ namespace PongMonogame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            //_spriteBatch.Draw(ballTexture, new Vector2(10, 10), Color.White);
-            //_spriteBatch.Draw(ballTexture, ballPosition, Color.White);
-            _spriteBatch.Draw(
-                ball2Texture,
-                ballPosition,
-                null,
-                Color.White,
-                0f,
-                new Vector2(ball2Texture.Width / 2, ball2Texture.Height / 2),
-                Vector2.One,
-                SpriteEffects.None,
-                0f
-            );
+            ball.Draw(_spriteBatch);
             _spriteBatch.End();
 
             base.Draw(gameTime);
@@ -50,26 +35,18 @@ namespace PongMonogame
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            _graphics.PreferredBackBufferWidth = 2000;
-            _graphics.PreferredBackBufferHeight = 1000;
             _graphics.ApplyChanges();
-
-            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
-                           _graphics.PreferredBackBufferHeight / 2);
-            ballSpeed = 800f;
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D ballTexture = Content.Load<Texture2D>("ball2");
+            ball = new Ball(ballTexture, _graphics);
 
-            // TODO: use this.Content to load your game content here
-            //ballTexture = Content.Load<Texture2D>("ball");
-            ball2Texture = Content.Load<Texture2D>("ball2");
-            soundEffect = Content.Load<SoundEffect>("GameMusic");
+            song = Content.Load<Song>("GameMusic");
+            MediaPlayer.Play(song);
         }
 
         protected override void Update(GameTime gameTime)
@@ -77,48 +54,7 @@ namespace PongMonogame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            var kstate = Keyboard.GetState();
-
-            if (kstate.IsKeyDown(Keys.W))
-            {
-                ballPosition.Y -= updatedBallSpeed;
-            }
-
-            if (kstate.IsKeyDown(Keys.S))
-            {
-                ballPosition.Y += updatedBallSpeed;
-            }
-
-            if (kstate.IsKeyDown(Keys.A))
-            {
-                ballPosition.X -= updatedBallSpeed;
-            }
-
-            if (kstate.IsKeyDown(Keys.D))
-            {
-                ballPosition.X += updatedBallSpeed;
-            }
-
-            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ball2Texture.Width / 2)
-            {
-                ballPosition.X = _graphics.PreferredBackBufferWidth - ball2Texture.Width / 2;
-            }
-            else if (ballPosition.X < ball2Texture.Width / 2)
-            {
-                ballPosition.X = ball2Texture.Width / 2;
-            }
-
-            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ball2Texture.Height / 2)
-            {
-                ballPosition.Y = _graphics.PreferredBackBufferHeight - ball2Texture.Height / 2;
-            }
-            else if (ballPosition.Y < ball2Texture.Height / 2)
-            {
-                ballPosition.Y = ball2Texture.Height / 2;
-            }
+            ball.Update(gameTime);
 
             base.Update(gameTime);
         }
---
+++ Models/Ball.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongMonogame.Models
+{
+    public class Ball
+    {
+        private GraphicsDeviceManager _graphics;
+        private float Speed;
+        private Texture2D Texture;
+        private Vector2 Velocity;
+
+        // Dirección y velocidad
+        public Ball(Texture2D texture, GraphicsDeviceManager graphics)
+        {
+            Texture = texture;
+            _graphics = graphics;
+            Speed = 500f;
+
+            // Posición inicial en el centro
+            Position = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+
+            // Dirección inicial (puedes ajustarla)
+            Velocity = new Vector2(1f, 1f);  // Se moverá diagonalmente al principio
+            Velocity.Normalize();  // Asegura que la velocidad esté normalizada
+        }
+
+        public Vector2 Position { get; private set; }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                Texture,
+                Position,
+                null,
+                Color.White,
+                0f,
+                new Vector2(Texture.Width / 2, Texture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            float updatedSpeed = Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Actualizar posición
+            Position += Velocity * updatedSpeed;
+
+            // Detectar colisión con los bordes
+            if (Position.X <= Texture.Width / 2 || Position.X >= _graphics.PreferredBackBufferWidth - Texture.Width / 2)
+            {
+                Velocity.X *= -1;  // Cambiar dirección horizontal
+            }
+
+            if (Position.Y <= Texture.Height / 2 || Position.Y >= _graphics.PreferredBackBufferHeight - Texture.Height / 2)
+            {
+                Velocity.Y *= -1;  // Cambiar dirección vertical
+            }
+        }
+    }
+}
